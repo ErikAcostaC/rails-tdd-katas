d878be9c78df33ba621ed3f7cbaa6cbc3ec7f5f6
Erik Acosta <erik.acosta.1@outlook.com>
2022-04-21 21:13:16 -0600

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/health', to: 'health#health'
+
+  resources :posts, only: [:index, :show, :create, :update]
+end
